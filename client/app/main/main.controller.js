0e79b3732ff486fa2212254b4e2954806877457f
Diego Moura <diego@diego-Vostro-3560.(none)>
2014-12-21 16:13:11 -0200

    subindo

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('mapasApp')
+  .controller('MainCtrl', function ($scope, $http, socket) {
+    $scope.awesomeThings = [];
+
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+      socket.syncUpdates('thing', $scope.awesomeThings);
+    });
+
+    $scope.addThing = function() {
+      if($scope.newThing === '') {
+        return;
+      }
+      $http.post('/api/things', { name: $scope.newThing });
+      $scope.newThing = '';
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+
+
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('thing');
+    });
+  });
